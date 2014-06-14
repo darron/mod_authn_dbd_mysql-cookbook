a2059e755b1aadb1419fea66796e0b2a99032d6c
Darron Froese <darron@froese.org>
2014-06-14 13:16:52 -0600

    First commit.

---
+++ metadata.rb
@@ -0,0 +1,10 @@
+# encoding: utf-8
+name             'mod_authn_dbd_mysql'
+maintainer       'Darron Froese'
+maintainer_email 'darron@froese.org'
+license          'Apache 2.0'
+description      'Installs/configures mod_authn_dbd_mysql'
+version          '0.1.0'
+recipe           'mod_authn_dbd_mysql::default', 'Installs/configures mod_authn_dbd_mysql'
+
+depends 'chef-sugar'
